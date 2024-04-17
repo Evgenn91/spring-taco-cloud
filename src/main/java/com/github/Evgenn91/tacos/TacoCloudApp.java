4ed71f8d4c6454eaf88da510ac73d53661f2829a
evgen <evgen@pochta.ru>
2024-04-17 14:37:32 +0500

    second commit

--- src/main/java/com/github/Evgenn91/tacos/TacoCloudApp.java
+++ src/main/java/com/github/Evgenn91/tacos/TacoCloudApp.java
@@ -1,2 +1,31 @@
-package com.github.Evgenn91.tacos;public class TacoCloudApp {
+package com.github.Evgenn91.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Данная аннотация объединяет 3 аннотации
+ * 1. @SpringBootConfiguration – определяет этот класс как класс кон-
+ * фигурации. В данный момент в этом классе не определяется
+ * никаких конфигурационных параметров, но если понадобится,
+ * в него можно добавить настройки Spring Framework. Эта аннота-
+ * ция, по сути, является специализированной формой аннотации
+ * @Configuration;
+ *
+ * 2. @EnableAutoConfiguration – включает автоконфигурацию Spring
+ * Boot. Подробнее об автоконфигурации мы поговорим позже,
+ * а пока просто имейте в виду, что эта аннотация сообщает Spring
+ * Boot о необходимости автоматически настраивать любые ком-
+ * поненты, которые могут вам понадобиться;
+ *
+ * 3. @ComponentScan – включает сканирование компонентов. Меха-
+ * низм сканирования позволяет объявлять другие классы с анно-
+ * тациями, такими как @Component, @Controller и @Service, чтобы
+ * фреймворк Spring автоматически обнаруживал и регистрировал
+ * их как компоненты в контексте приложения Spring.
+ * */
+@SpringBootApplication
+public class TacoCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApp.class, args);
+    }
 }
--- src/test/java/com/github/Evgenn91/tacos/TacoCloudAppTests.java
+++ src/test/java/com/github/Evgenn91/tacos/TacoCloudAppTests.java
@@ -1,2 +1,19 @@
-package com.github.Evgenn91.tacos;public class TacoCloudAppTests {
+package com.github.Evgenn91.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/* Аннотация @SpringBootTest настраивает JUnit на запуск теста с под-
+    держкой возможностей Spring Boot. Подобно @SpringBootApplication,
+    @SpringBootTest – это составная аннотация, которая сама снабжена
+    аннотацией @ExtendWith(SpringExtension.class), добавляющей под-
+    держку возможностей тестирования Spring в JUnit 5. */
+@SpringBootTest
+public class TacoCloudAppTests {
+    /* этот тестовый класс
+        выполняет важную проверку – успешность загрузки контекста
+        приложения Spring.*/
+    @Test
+    public void contextLoads() {
+    }
 }
